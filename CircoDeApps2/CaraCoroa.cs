3790b6b8d7ecbef7b37a730eded73265f98f07c1
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2022-08-29 17:31:37 -0300

    Add files via upload

---
+++ CircoDeApps2/CaraCoroa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircoDeApps2
+{
+    public partial class CaraCoroa : Form
+    {
+        public CaraCoroa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int sorteio = Convert.ToInt32(rnd.Next(2));
+            /*switch (sorteio) {
+                case 0:
+                    pbxResultado.image = Properties.Resources.moeda_cara;
+                    lblResultado.Text = "Cara";
+                    break;
+                case 1:
+                    pbxResultado.Image = Properties.Resources.moeda_coroa;
+                    lblResultado.Text = "Coroa";
+                    break;
+            }*/
+        }
+    }
+}
